667aeaee1a52f1a5e8bc1e97057b3a695a9a54f6
minhnguyennhat <144748251+MinhNguyenNhat0612@users.noreply.github.com>
2024-10-11 01:26:43 +0700

    Done the logic part as in the doc

--- app/src/main/java/com/example/lab_12_fragment/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/lab_12_fragment/ui/dashboard/DashboardFragment.java
@@ -4,34 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.lab_12_fragment.databinding.FragmentDashboardBinding;
+import com.example.lab_12_fragment.viewmodel.SharedViewModel;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private SharedViewModel sharedViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.btnUppNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sharedViewModel.incrementData();
+            }
+        });
+
+        binding.btnDownNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sharedViewModel.decreaseData();
+            }
+        });
+
+        sharedViewModel.getSharedData().observe(getViewLifecycleOwner(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer data) {
+                binding.textDashboard.setText(String.valueOf(data));
+            }
+        });
         return root;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
     }
 }
--- app/src/main/java/com/example/lab_12_fragment/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/lab_12_fragment/ui/home/HomeFragment.java
@@ -8,24 +8,29 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.lab_12_fragment.databinding.FragmentHomeBinding;
+import com.example.lab_12_fragment.viewmodel.SharedViewModel;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private SharedViewModel sharedViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+
+        sharedViewModel.getSharedData().observe(getViewLifecycleOwner(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer data) {
+                binding.textHome.setText(String.valueOf(data));
+            }
+        });
         return root;
     }
 
